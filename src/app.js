c83c3f906f8adc131242bdc588cd60032b5326b0
Lauri Mäkinen <lauri@mits.fi>
2017-05-13 15:02:56 +0300

    React versio upgrade. Includes prop-types change

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from "react"
+import React from "react"
+import PropTypes from "prop-types"
 import "./styles/app.scss"
 import "../node_modules/font-awesome/css/font-awesome.css"
 
