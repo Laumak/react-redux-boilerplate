6a9a4f43efef016e4fd2e9d3f2a2d73314ff82d4
Lauri Mäkinen <lauri@mits.fi>
2017-05-21 12:34:01 +0300

    Extract more vendor packages

--- src/vendor.js
+++ src/vendor.js
@@ -1,4 +1,16 @@
 /* eslint-disable no-unused-vars */
 
-import React from "react"
-import ReactDOM from "react-dom"
+import React        from "react"
+import ReactDOM     from "react-dom"
+import { Provider } from "react-redux"
+
+import {
+  bindActionCreators,
+  combineReducers,
+  createStore,
+  applyMiddleware,
+} from "redux"
+
+import { AppContainer }         from "react-hot-loader"
+import CSSModules               from "react-css-modules"
+import { BrowserRouter, Route } from "react-router-dom"
