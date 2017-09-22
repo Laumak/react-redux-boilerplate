fb1ffab98e922204dc09836227bc3eb02a4071e7
Lauri Mäkinen <lauri@mits.fi>
2017-09-22 20:26:40 +0300

    VSCode Chrome debugging

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from "redux"
+import { createStore, applyMiddleware, compose } from "redux"
 
 import reduxImmutableStateInvariant from "redux-immutable-state-invariant"
 import thunk from "redux-thunk"
@@ -6,7 +6,7 @@ import thunk from "redux-thunk"
 import rootReducer from "../reducers"
 
 export default (initialState) => {
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
   const store = createStore(
     rootReducer,
