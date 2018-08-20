d81beb61a7a04b8c21d736f04f6bc2ec46d3018e
Lauri Mäkinen <lauri.makinen@digia.com>
2018-08-20 11:29:24 +0300

    React 16.4 upgrade

--- buildScripts/testSetup.js
+++ buildScripts/testSetup.js
@@ -1,5 +1,13 @@
+global.requestAnimationFrame = function(callback) {
+  setTimeout(callback, 0)
+}
+
 // Compile code to ES2015 with Babel before running tests.
 require("babel-register")()
+var enzyme = require("enzyme")
+var Adapter = require("enzyme-adapter-react-16")
 
 // During tests, ignore file types which Enzyme can't work with.
 require.extensions[".css", ".scss", ".sass", ".jpg", ".png"] = function() {}
+
+enzyme.configure({ adapter: new Adapter() })
